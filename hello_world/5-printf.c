199b4110ecb06791ef4c5181567aeac36357ac31
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-02-17 07:24:44 -0800

    5-printf.c

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - "function"
- * description - with proper grammar, but the outcome is a piece of art,
+ * main - C program that prints exactly with proper grammar
  *
- * Return 0;
+ * Return: 0;
  */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return  (0) ;	
+return (0);
 }

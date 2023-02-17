77a785b3cac271506b26067c831e6963a0e0baf0
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-02-17 05:33:18 -0800

    4-puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#include <stdio.h>
+
+/**
+ * main-function
+ * description
+ *
+ * return: always 0
+ */
+int main(void)
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+
+}

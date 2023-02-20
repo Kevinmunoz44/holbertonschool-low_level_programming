0f3e3ebad40d7948bd13541039b2983755dda4d8
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-02-20 14:23:38 -0800

    3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -10,11 +10,13 @@ int main(void)
 {
 char lowercase, uppercase;
 for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
-
-for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
-
+{
 putchar(lowercase);
+}
+for (uppercase = 'A'; uppercase <= 'Z'; uppercase++)
+{
 putchar(uppercase);
+}
 putchar('\n');
 return (0);
 }

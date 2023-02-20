09ff14f2026bd943f3a9fecb6ebc8f18f14f804e
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-02-20 08:34:20 -0800

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,13 @@
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /* more headers goes there */
-
+/**
+* main - print the values of n
+* positive negative and zero
+*
+* Return 0;
+*/
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -10,17 +16,15 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 if (n > 0)
 {
-printf("The number is greater");	
+printf("%d is positive");
 };
 else if (n < 0)
 {
-printf("The number is less");
+printf("%d is negative\n");
 };
 else
 {
-pintf("The number is 0")
+pintf("%d is zero\n")
 };
 return (0);
 };
-
-

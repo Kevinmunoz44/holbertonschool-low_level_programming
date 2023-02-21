5777e690d7dc59bd8e545e2e19c953746132d896
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-02-21 10:42:00 -0800

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -12,3 +12,14 @@ int main(void)
 print_alphabet();
 return (0);
 }
+
+void print_alphabet(void)
+{
+char alphabet;
+for (alphabet = 'a'; alphabet < 'z'; alphabet++)
+{
+putchar(alphabet);
+}
+putchar('\n');
+}
+
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,9 +1,1 @@
-void print_alphabet(void)
-{
-char alphabet;
-for (alphabet = 'a'; alphabet < 'z'; alphabet++)
-{
-putchar(alphabet);
-}
-putchar('\n');
-}
+

eed25132336ec6be4efc5f7b0e745c4406c69108
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-02-21 07:55:31 -0800

    6-print_numberz.c

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print the numbers with single digit
+ *
+ *
+ * Return: 0;
+ */
+int main(void)
+{
+int n = 0;
+while (n < 10)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}

dc338046fbe8ce85ea4c1d461d989e02b7fee806
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-02-22 10:56:34 -0800

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit
+ * @digit: - save values
+ *
+ * Return: 0;
+ */
+int print_last_digit(int digit)
+{
+if (digit % 10)
+return (digit);
+}

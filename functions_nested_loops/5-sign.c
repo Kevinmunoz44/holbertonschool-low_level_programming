056234cd830e898df199fe6cbff7f8cd8d7af951
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-02-22 08:42:56 -0800

    5-sign.c

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -12,18 +12,24 @@ int print_sign(int n)
 if (n > 0)
 {
 _putchar('+');
+_putchar(',');
+_putchar(' ');
 _putchar('\n');
 return (1);
 }
 else if (n < 0)
 {
 _putchar('-');
+_putchar(',');
+_putchar(' ');
 _putchar('\n');
 return (-1);
 }
 else
 {
 _putchar('0');
+_putchar(',');
+_putchar(' ');
 _putchar('\n');
 return (0);
 }

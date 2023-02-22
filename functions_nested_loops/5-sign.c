9a376c9be85f299cdcffaa22df9de316f5c4591a
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-02-22 08:39:14 -0800

    5-sign.c

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_sign - print the signs of numbers
+ * @n: - save the signs of the numbers
+ *
+ * Return: 0;
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+_putchar('\n');
+return (1);
+}
+else if (n < 0)
+{
+_putchar('-');
+_putchar('\n');
+return (-1);
+}
+else
+{
+_putchar('0');
+_putchar('\n');
+return (0);
+}
+}

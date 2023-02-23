158615cc0092b49f5ec6b56d0a9dca9f72d61cd5
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-02-23 12:16:27 -0800

    1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdigit - print one digit
+ * @c: - save values
+ *
+ * Return: 0;
+ */
+int _isdigit(int c)
+{
+if (c >= 0 && c <= 9)
+return (1);
+else
+return (0);
+}

2175c3dda931e19de94fb1d17d79118c70369aa9
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-02-23 12:33:41 -0800

    2-mul.c

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - print multiplication
+ * @a: - save one value
+ * @b: - save second value
+ *
+ * Return: 0;
+ */
+int mul(int a, int b)
+{
+int result;
+result = a * b;
+return (result);
+}

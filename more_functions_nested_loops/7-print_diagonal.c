eacf646cce8eab1c4858981ee25ce56dcbce8dcc
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-02-24 05:40:32 -0800

    7-print_diagonal.c

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal
+ * @n: - save values
+ *
+ * Return: 0;
+ */
+void print_diagonal(int n)
+{
+int row, num_lines;
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (row = 1; row <= n; row++)
+{
+if (row > 1)
+{
+for (num_lines = 1; num_lines <= row - 1; num_lines++)
+{
+_putchar(' ');
+}
+}
+_putchar('\\');
+_putchar('\n');
+}
+}
+}

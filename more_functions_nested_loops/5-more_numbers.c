724ac8eaf23b890001a63aac57e8be15305bf137
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-02-24 05:20:07 -0800

    5-more_numbers.c

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - print the numbers of 0 the 14 10 times
+ *
+ * 
+ * Return: 0;
+ */
+void more_numbers(void)
+{
+int n, t, u, r;
+for (r = 1; r <= 10; r++)
+{
+for (n = 0; n <= 14; n++)
+{
+t = n / 10;
+u = n % 10;
+if (n > 9)
+_putchar(t + '0');
+_putchar(u + '0');
+}
+_putchar('\n');
+}
+}

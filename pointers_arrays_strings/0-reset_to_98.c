9156300078ab9d74cf2b9cde91386f6ab9438ad7
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-02-27 08:44:56 -0800

    -reset_to_98.c

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - 
+ * @n: - 
+ *
+ * Return: 0;
+ */
+void reset_to_98(int *n)
+{
+int p;
+int *n = &p;
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,10 @@
+
+/**
+* main - check the code
+*
+*
+* Return: 0;
+*/
+
+int _putchar(char c);
+void reset_to_98(int *n);

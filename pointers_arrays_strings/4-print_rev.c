a8e03a04d3ccddf8ed2a03011e7a27fa71c7af80
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-02-28 06:11:23 -0800

    4-print_rev.c

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_rev - print all in reverse
+ * @s: - save values
+ *
+ * Return: 0;
+ */
+void print_rev(char *s)
+{
+ int i, len, temp;
+  len = strlen(s);
+for (i = 0; i < len/2; i++)
+{
+temp = s[i];
+s[i] = s[len - i - 1];
+s[len - i - 1] = temp;
+} 
+}

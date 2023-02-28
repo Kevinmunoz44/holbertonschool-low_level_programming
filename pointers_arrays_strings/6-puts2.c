aa75253007eeab87e102bf3df9fac8f848b27a7e
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-02-28 10:43:30 -0800

    6-puts2.c

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - print other characters of a string
+ * @str: - save values
+ *
+ * Return: 0;
+ */
+void puts2(char *str)
+{
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if ((i % 2)==0)
+{
+_putchar(*(str + i));
+}
+}
+_putchar('\n');
+}

c2d9dbd14e25a39c794c7bcc0559c023e023992d
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-03-02 11:03:44 -0800

    3-strcmp.c

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strcmp - Your function should work exactly like strcmp
+ * @s1: - save values
+ * @s2: - save values
+ *
+ * Return: 0;
+ */
+int _strcmp(char *s1, char *s2)
+{
+for (; (*s1 != '\0' && *s2 != '\0') &&(*s1 == *s2); s1++, s2++)
+;
+if (*s1 == *s2)
+{
+return (0);
+}
+return (*s1 - *s2);
+}

a40062985bf3bd6edb77b39ce348372825d48b0d
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-03-02 17:00:46 -0800

    5-string_toupper.c

---
+++ pointers_arrays_strings/5-2main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char s[] = "Look up!\n";
+    char *ptr;
+
+    ptr = string_toupper(s);
+    printf("%s", ptr);
+    printf("%s", s);
+    return (0);
+}

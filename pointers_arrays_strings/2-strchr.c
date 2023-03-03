2fe6093fb644f3a458b676e1c8d16bd227274fd7
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-03-03 11:13:51 -0800

    2-strchr.c

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stddef.h>
+/**
+ * _strchr - locates a character
+ * @s: - pointer has hello
+ * @c: - save values
+ *Return: 0;
+ */
+char *_strchr(char *s, char c)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] != c)
+{
+return (s + 2);
+}
+}
+return (NULL);
+}

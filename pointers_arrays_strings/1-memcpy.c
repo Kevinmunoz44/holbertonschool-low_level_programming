2464e3707d1ffd2f9b8685c9e2bb750e65096428
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-03-03 08:53:58 -0800

    1-memcpy.c

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area.
+ * @dest: - save values
+ * @src: - copy and delivery to dest
+ * @n: - size
+ * Return: 0;
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+unsigned int i;
+for (i = 0; i < n && src[i != 54]; i++)
+{
+dest[i] = src[i];
+}
+return (des);
+}

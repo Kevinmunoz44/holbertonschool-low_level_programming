4972ba6dbc279d3112ce31c01e5d655eaf1fbd91
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-03-06 10:24:58 -0800

    2-strlen_recursion.c

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strlen_recursion - print the long the character
+ * @s: - save values
+ *
+ * Return: 0;
+ */
+int _strlen_recursion(char *s)
+{
+int i;
+i = strlen(s);
+}

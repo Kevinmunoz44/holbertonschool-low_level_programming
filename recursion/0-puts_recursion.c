c44e392b2a744c6a8a50217224cb439b3d290c60
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-03-06 07:19:55 -0800

    0-puts_recursion.c

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - print a string
+ * @s: - save values
+ *
+ * Return: 0;
+ */
+void _puts_recursion(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+;
+puts(s);
+}

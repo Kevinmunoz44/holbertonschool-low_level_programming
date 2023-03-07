27b79f7e6d0a768b9ba25cb80f570c9009f3b5b2
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-03-07 09:02:29 -0800

    libmy.a, main.h

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,11 +1,5 @@
-
-/**
-* main - check the code
-*
-*
-* Return: 0;
-*/
-
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -26,3 +20,4 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+#endif

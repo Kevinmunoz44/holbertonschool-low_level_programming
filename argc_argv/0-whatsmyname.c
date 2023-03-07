d71567289a9c5e927eeb793303776c4d6e677516
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-03-07 11:33:47 -0800

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - print the name 
+ * main - print the name
  * @argc: - count array
  * @argv: - array
  *
  * Return: 0;
  */
-int main (int __attribute__((unused))argc, char *argv[])
+int main(int __attribute__((unused))argc, char *argv[])
 {
 printf("%s\n", argv[0]);
 return (0);

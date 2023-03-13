5dbf019e438b56f92426d129b38d49c2028f4540
kevinmunoz44 <kevinmunozb04@gmail.com>
2023-03-13 10:18:08 -0700

    1-strdup.c

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - copy a string and send a pointer
+ * @str: - pointer save values
+ *
+ * Return: 0;
+ */
+char *_strdup(char *str)
+{
+int i, f;
+char *k;
+if (str == NULL)
+{
+return (NULL);
+}
+for (i = 1; i < str[i]; i++)
+;
+k = malloc((sizeof(char) * i) + 1);
+if (k == NULL)
+{
+return (NULL);
+}
+for (f = 0; f < i; f++)
+{
+k[f] = str[f];
+}
+k[f] = '\0';
+return (k);
+}
